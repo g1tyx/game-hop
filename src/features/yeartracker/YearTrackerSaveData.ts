7059da0b48d04f937640cb8d8398d4474fff2dcf
Isha Dijcks <ishadijcks@gmail.com>
2020-08-07 21:40:31 +0200

    Add YearTracker.ts

---
+++ src/features/yeartracker/YearTrackerSaveData.ts
@@ -0,0 +1,10 @@
+export class YearTrackerSaveData {
+    month: number;
+    monthProgress: number;
+
+
+    constructor(month: number, monthProgress: number) {
+        this.month = month;
+        this.monthProgress = monthProgress;
+    }
+}
