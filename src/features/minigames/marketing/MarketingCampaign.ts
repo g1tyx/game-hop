aea50082f74103121e5c1f180d3e7dd5b56a24d7
Isha Dijcks <ishadijcks@gmail.com>
2020-08-08 19:37:18 +0200

    Add marketing campaigns

---
+++ src/features/minigames/marketing/MarketingCampaign.ts
@@ -0,0 +1,57 @@
+import * as ko from "knockout";
+import {Currency} from "../../wallet/Currency";
+import {App} from "../../../App";
+
+export class MarketingCampaign {
+    public monthsToComplete: number;
+    public cost: Currency;
+    public fameReward: number;
+
+    private readonly _completionProgress: ko.Observable<number>;
+    private readonly _isStarted: ko.Observable<boolean>;
+
+    constructor(monthsToComplete: number, cost: Currency, fameReward: number) {
+        this.monthsToComplete = monthsToComplete;
+        this.cost = cost;
+        this.fameReward = fameReward;
+
+        this._completionProgress = ko.observable(0);
+        this._isStarted = ko.observable(false);
+    }
+
+    public tryToStart(): boolean {
+        if (!App.game.wallet.hasCurrency(this.cost)) {
+            return false;
+        }
+
+        App.game.wallet.loseCurrency(this.cost);
+        this.start();
+        return true;
+    }
+
+    public isCompleted(): boolean {
+        return this.completionProgress >= this.monthsToComplete;
+    }
+
+    private start(): void {
+        this.isStarted = true;
+    }
+
+    // Knockout getters/setters
+    get completionProgress(): number {
+        return this._completionProgress();
+    }
+
+    set completionProgress(value: number) {
+        this._completionProgress(value);
+    }
+
+    get isStarted(): boolean {
+        return this._isStarted();
+    }
+
+    set isStarted(value: boolean) {
+        this._isStarted(value);
+    }
+
+}
--- src/features/minigames/marketing/MarketingMiniGame.ts
+++ src/features/minigames/marketing/MarketingMiniGame.ts
@@ -3,6 +3,11 @@ import {Feature} from "../../../engine/Feature";
 import {Requirement} from "../../../engine/requirements/Requirement";
 import {MarketingMiniGameSaveData} from "./MarketingMiniGameSaveData";
 import {MarketingFameRequirement} from "./MarketingFameRequirement";
+import {MarketingCampaign} from "./MarketingCampaign";
+import {Currency} from "../../wallet/Currency";
+import {CurrencyType} from "../../wallet/CurrencyType";
+import {App} from "../../../App";
+import {ISimpleEvent, SimpleEventDispatcher} from "ste-simple-events";
 
 export class MarketingMiniGame extends Feature implements MiniGame {
     name: string = "Marketing";
@@ -10,14 +15,68 @@ export class MarketingMiniGame extends Feature implements MiniGame {
     yearRequirements: Requirement[];
 
     fame: number;
+    availableCampaigns: MarketingCampaign[];
+
+    private readonly _onCampaignCompletion = new SimpleEventDispatcher<MarketingCampaign>();
+
 
     constructor() {
         super();
         this.yearRequirements = [];
+        this.availableCampaigns = [];
     }
 
     initialize(): void {
         this.yearRequirements.push(new MarketingFameRequirement(1000));
+
+        App.game.yearTracker.onMonthStart.subscribe(() => this.spawnCampaign());
+    }
+
+    /**
+     * Start the campaign, returns if this was successful
+     */
+    startCampaign(index: number): boolean {
+        if (this.availableCampaigns[index] == null) {
+            console.warn(`Could not start campaign with index ${index}`);
+            return false;
+        }
+        return this.availableCampaigns[index].tryToStart();
+    }
+
+    removeCampaign(campaign: MarketingCampaign): void {
+        const index = this.availableCampaigns.indexOf(campaign, 0);
+        if (index > -1) {
+            this.availableCampaigns.splice(index, 1);
+        }
+    }
+
+    spawnCampaign(): void {
+        this.availableCampaigns.push(new MarketingCampaign(0.5, new Currency(100, CurrencyType.money), 100))
+    }
+
+    update(delta: number): void {
+        for (const campaign of this.availableCampaigns) {
+            if (campaign.isStarted) {
+                campaign.completionProgress += App.game.yearTracker.secondsToMonthPercentage(delta);
+
+                if (campaign.isCompleted()) {
+                    this.complete(campaign);
+                }
+            }
+        }
+    }
+
+    complete(campaign: MarketingCampaign): void {
+        this.fame += campaign.fameReward;
+        this._onCampaignCompletion.dispatch(campaign);
+        this.removeCampaign(campaign);
+
+    }
+
+
+    reset(): void {
+        this.fame = 0;
+        this.availableCampaigns = [];
     }
 
     load(data: MarketingMiniGameSaveData): void {
@@ -27,11 +86,15 @@ export class MarketingMiniGame extends Feature implements MiniGame {
         return undefined;
     }
 
-    reset(): void {
-    }
-
     save(): MarketingMiniGameSaveData {
         return undefined;
     }
+
+
+    // Event getters/setters
+    public get onCampaignCompletion(): ISimpleEvent<MarketingCampaign> {
+        return this._onCampaignCompletion.asEvent();
+    }
+
 
 }
